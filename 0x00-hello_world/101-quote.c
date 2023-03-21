689ba0fe8ff112b7b46b718ee5e52e5eae30c46e
Tasneem@205 <tasneemabdeltawab205@gmail.com>
2023-03-20 18:25:59 -0700

    8.2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,7 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int write(int filedes, const char *buf, unsigned int nbyte); 
+int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
