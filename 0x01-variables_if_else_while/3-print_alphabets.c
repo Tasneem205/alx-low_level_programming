3e2b0c5d3c24df2cf0f7a7eddc9cb2ef0c4f21df
Tasneem@205 <tasneemabdeltawab205@gmail.com>
2023-03-20 18:49:39 -0700

    1.1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char low;
+
+for (low = 'a'; low <= 'z'; low++)
+  printf("%c",low);
+for (low = 'A'; low <= 'Z'; low++)
+  printf("5c",low);
+ printf("\n");
+
+return (0);
+}
