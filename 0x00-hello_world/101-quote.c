d6bf808ea74a14f5e28dce164128d11056cb0a7a
Tasneem@205 <tasneemabdeltawab205@gmail.com>
2023-03-20 18:20:47 -0700

    8.1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <sys/syscall.h>
 
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
+int write(int filedes, const char *buf, unsigned int nbyte); 
 int main(void)
 {
 const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-syscall(SYS_write, 1, msg, sizeof(msg));
+write(1, msg, sizeof(msg));
 return (1);
 }
