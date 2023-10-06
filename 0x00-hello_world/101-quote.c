675dd8247bc08640d33c1d9a0df73c63fa28ce51
Tasneem205 <tasneemabdeltawab205@gmail.com>
2023-10-05 19:10:53 -0700

    last try

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,14 @@
 /**
  * main - Entry point
  * Return: Always 1 (Success)
+ * @filedes: is the number of fields in the input
+ * @buf: is the buffer
+ * @nbyte: is the size of the buffer in bytes
  */
 int write(int filedes, const char *buf, unsigned int nbyte);
 int main(void)
 {
 char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(1, msg, sizeof(msg));
+write(2, msg, 59);
 return (1);
 }
