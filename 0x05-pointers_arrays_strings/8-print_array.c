4ebb86a7480d34291d491cf0d46194f38ca69a9b
Tasneem205 <tasneemabdeltawab205@gmail.com>
2023-10-16 18:44:27 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+/**
+ * print_array - prints the array followed by a new line
+ * @a: the array
+ * @n: the size of the array
+ * Returns: nothing
+ */
+
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	printf("%d \n", a[i]);
+}
