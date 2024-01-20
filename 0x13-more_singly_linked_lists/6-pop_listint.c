fb9d9d766d110a3cd40768c18a6b3c85055118ef
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-01-19 16:00:30 -0800

    6.1

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -15,7 +15,7 @@ int pop_listint(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return (0);
 
-	content = (head)->n;
+	content = (*head)->n;
 	popped = *head;
 	*head = (*head)->next;
 	free(popped);
