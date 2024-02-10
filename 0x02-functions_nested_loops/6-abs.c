6c01c4a9a76bd2f28ec929eba1d53b6a91639138
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-02-09 18:13:02 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs- returns the abs value of int
+ *
+ * @n: the number
+ * Return: the abs value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
