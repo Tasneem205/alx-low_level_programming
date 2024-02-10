54d2cd531429cbc8918fb43845b7b0ec8c0a7d55
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-02-10 15:08:09 -0800

    file name fiz

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -13,13 +13,13 @@ int main(void)
 
 	k = 2;
 
-	for (i = 1; i <= 50; ++i)
+	for (i = 1; i < 50; ++i)
 	{
 		printf("%ld, ", j);
 		next = j + k;
 		j = k;
 		k = next;
 	}
-	printf("\n");
+	printf("%ld\n", j);
 	return (0);
 }
