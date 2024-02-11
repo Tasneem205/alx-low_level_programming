86d7fd4fd951b1c5d8e42bbb81061ef70ba420ad
Tasneem205 <tasneemabdeltawab205@gmail.com>
2024-02-10 19:33:49 -0800

    8.14

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,17 @@
 
 void print_array(int *a, int n)
 {
-	int i;
+	int i, len;
 
-	for (i = 0; i < n - 1; i++)
+	for (len = 0; a[len] != '\0'; len++)
+
+	if (len > n && n >= 0)
 	{
-		printf("%d, ", a[i]);
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf("%d", a[i]);
 	}
-	printf("%d\n", a[i]);
+	printf("\n");
 }
