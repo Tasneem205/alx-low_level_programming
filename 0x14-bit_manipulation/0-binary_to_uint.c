2b65d0d896d8420df35b71207960be39b8472702
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-02 18:31:45 +0200

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,9 @@ unsigned int binary_to_uint(const char *b)
 	res = 0;
 	strlen = 0;
 
+	if (b == NULL)
+		return (0);
+
 	while (b[strlen] != '\0')
 		strlen++;
 
@@ -33,5 +36,5 @@ unsigned int binary_to_uint(const char *b)
 		}
 		strlen--;
 	}
-	return res;
+	return (res);
 }
