afaf6ad9fa041990b5045d8fdb7feed01943ce88
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-29 04:16:46 +0200

    task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""mesures an islan permiter"""
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island in the grid.
+
+    Args:
+        grid (list): 2d list
+    Returns:
+        int
+    """
+    w = len(grid[0])
+    h = len(grid)
+    edges = 0
+    size = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                if (j > 0 and grid[i][j-1] == 1):
+                    edges += 1
+            if (j > 0 and grid[i-1][j] == 1):
+                edges += 1
+    return size * 4 - edges * 2
