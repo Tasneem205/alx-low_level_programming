e13c2f077fc3c891ccdad9a85ae2bd6ad5cf0407
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-29 04:30:02 +0200

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -18,6 +18,7 @@ def island_perimeter(grid):
     for i in range(h):
         for j in range(w):
             if grid[i][j] == 1:
+                size += 1
                 if (j > 0 and grid[i][j-1] == 1):
                     edges += 1
                 if (i > 0 and grid[i-1][j] == 1):
