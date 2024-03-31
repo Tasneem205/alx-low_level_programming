f1cbe96872452e1bfc09bcde2f4be280d28323c9
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-03-31 19:50:21 +0200

    task 7 2

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,14 +7,19 @@
 
 char *leet(char *str)
 {
-	int index = 0;
+	int index1 = 0, index2 = 0;
 
-	while (str[index])
+	char *arr = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char *lee = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	while (str[index1])
 	{
-		if (str[index] == 'a' || str[index] == 'A')
-			str[index] = '4';
-		else if (str[index] == 'e' || )
-		index++;
+		for (index2 = 0' index2 < 10; index2++)
+		{
+			if (str[index1] == arr[index2])
+				str[index1] = lee[index2];
+		}
+		index1++;
 	}
 	return (str);
 }
