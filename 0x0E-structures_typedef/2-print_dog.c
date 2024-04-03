84c7cca1df1acfd6cdd5244d328ac94d00d838f0
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-04-03 08:30:04 +0200

    fix 2-print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,7 +3,7 @@
 
 /**
  * print_dog - prints a dog with it's params
- * @dog: the dog structure
+ * @d: the dog structure
  * Return: null
  */
 void print_dog(struct dog *d)
@@ -18,6 +18,6 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", d->name);
 		}
-		printf("Age: %f\nOnwer: %s\n", d->age, d->owner);
+		printf("Age: %f\nOwner: %s\n", d->age, d->owner);
 	}
 }
