9cf1d2581da46dce4a82d08a60480aeb6150cef6
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-04-12 08:46:29 +0200

    task 2.2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - function that prints numbers, follwed by a new line
+ * print_strings - function that prints numbers, follwed by a new line
  * @seperator: string acts as a divisor
  * @n: count of the variables
 */
@@ -20,7 +20,7 @@ void print_strings(const char *seperator, const unsigned int n, ...)
 	{
 		s = va_arg(ap, char *);
 		if (s == NULL)
-			s = "nil";
+			s = "(nil)";
 		printf("%s", s);
 		if (i != n - 1 && seperator != NULL)
 			printf("%s", seperator);
