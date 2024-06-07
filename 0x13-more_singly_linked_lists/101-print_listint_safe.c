07f594ebf7cf6fcca97def840629d80049f4f46b
Tasneem205 <tasneemabdeltawab295@gmail.com>
2024-06-07 21:21:41 +0300

    task 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints all the elements of a linked list
+ * @head: head of the list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *cursor = head;
+	size_t count = 0;
+	long int diff;
+
+	while (cursor != NULL)
+	{
+		diff = cursor - cursor->next;
+		count += 1;
+		printf("[%p] %d\n", (void *)cursor, cursor->n);
+		if (diff > 0)
+		{
+			cursor = cursor->next;
+		}
+		else
+		{
+			printf("->[%p] %d\n", (void *)cursor->next, cursor->next->n);
+			break;
+		}
+	}
+	return (count);
+}
